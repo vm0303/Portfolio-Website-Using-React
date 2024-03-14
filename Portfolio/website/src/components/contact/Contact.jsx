2380aa3546b841b47ad76c3b3caf3fba375a1d59
Vishal Madhav <57302592+vm0303@users.noreply.github.com>
2024-03-13 23:13:44 -0400

    Update on 3/13/2024
    - Found a workaround on scroll in Contact.jsx
    - Fixed the review content

--- Portfolio/website/src/components/contact/Contact.jsx
+++ Portfolio/website/src/components/contact/Contact.jsx
@@ -2,40 +2,38 @@ import React, {useRef, useState, useEffect} from 'react';
 import './Contact.css';
 import Avatar from '../../image/Vishal-Contact.jpg';
 import {Fade} from 'react-reveal';
-
-
-import {Slide, toast, ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import InputForm from './InputForm';
+import {Slide, ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = ({setMenuOpen}) => {
-
     const [focused, setFocus] = useState(false);
-
+    const [scrollLimit, setScrollLimit] = useState(2150);
     const handleFocus = () => {
         setFocus(true);
-
     };
     const handleInputClick = () => {
         setMenuOpen(false);
     };
 
-
     const [vals, setVals] = useState({
         user_name: '',
         user_subject: '',
         user_email: '',
+        message: '' // Added message field
     });
 
+    const containerReviewRef = useRef(null);
+    const contactContainerRef = useRef(null);
+    const [reviewActive, setReviewActive] = useState(false);
 
-    const inputs = [
+    const [inputs, setInputs] = useState([
         {
             id: 1,
             name: 'user_name',
             type: 'text',
             placeholder: 'Enter your name here. ',
-            errorMsg:
-                "Please enter a valid first name or a full name with a space that is between 2-50 characters. " +
+            errorMsg: "Please enter a valid first name or a full name with a space that is between 2-50 characters. " +
                 "Avoid using numbers or any special characters that are not typically used in names." +
                 "In addition, make sure you don't start with a whitespace.",
             label: 'Name',
@@ -47,8 +45,7 @@ const Contact = ({setMenuOpen}) => {
             name: 'user_subject',
             type: 'text',
             placeholder: 'Enter the message subject here.',
-            errorMsg:
-                'Please enter a valid subject title that can summarize your message here. ' +
+            errorMsg: 'Please enter a valid subject title that can summarize your message here. ' +
                 'Make sure it is between 2-78 characters, and that it does not start with a special character, ' +
                 'or a whitespace.',
             label: 'Subject',
@@ -60,45 +57,91 @@ const Contact = ({setMenuOpen}) => {
             name: 'user_email',
             type: 'email',
             placeholder: 'Enter your email here.',
-            errorMsg:
-                'Please enter a valid email address. This is so I can reply my response back to you.',
+            errorMsg: 'Please enter a valid email address. This is so I can reply my response back to you.',
             label: "Email",
             required: true,
         },
-    ];
+    ]);
 
     const formRef = useRef();
 
-    /*The code below handles the logic when the modal is open*/
-
-
     const handleReview = async (event) => {
         event.preventDefault();
-
     }
 
     const handleChange = (e) => {
         const {name, value} = e.target;
+        setVals(prevVals => ({
+            ...prevVals,
+            [name]: value,
+        }));
+    };
 
-        // Handle message separately
-        if (name === 'message') {
-            setVals((prevVals) => ({
-                ...prevVals,
-                [name]: value,
-            }));
-        } else {
-            setVals((prevVals) => ({
-                ...prevVals,
-                [name]: value,
-            }));
+    useEffect(() => {
+        if (containerReviewRef.current && reviewActive) {
+            containerReviewRef.current.scrollIntoView({behavior: 'smooth'});
         }
+    }, [reviewActive]);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (reviewActive) {
+                // Check if the user is scrolling beyond the scroll limit
+                if (window.pageYOffset < scrollLimit) {
+                    // If so, prevent further scrolling
+                    window.scrollTo(0, scrollLimit);
+                }
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [scrollLimit, reviewActive]);
+
+
+    const handleBackButtonClick = () => {
+        // Scroll to the "contact" element
+        if (contactContainerRef.current) {
+            contactContainerRef.current.scrollIntoView({behavior: 'smooth'});
+        }
+        // Trigger the fade-out animation by setting reviewActive to false
+        setReviewActive(false);
+    };
+
+    const handleReviewButtonClick = () => {
+        // Check if all required fields are filled
+        const hasEmptyFields = inputs.some(input => {
+            return !vals[input.name] && input.required;
+        });
+
+        // If there are empty required fields, display errors and prevent opening the review section
+        if (hasEmptyFields) {
+            // Display errors for empty required fields
+            const updatedInputs = inputs.map(input => {
+                if (!vals[input.name] && input.required) {
+                    return {...input, error: true};
+                }
+                return input;
+            });
+            setInputs(updatedInputs);
+            return; // Exit early, do not proceed to opening the review section
+        }
+
+        // Proceed to open the review section if all required fields are filled
+        if (containerReviewRef.current) {
+            containerReviewRef.current.scrollIntoView({behavior: 'smooth'});
+        }
+        setReviewActive(true);
     };
 
     return (
         <Fade effect="fade" delay={700}>
-            <div className='container fade-out' id="contact">
+            <div className='container' id="contact">
                 <div className="container-bg"></div>
-                <div className="container-wrapper">
+                <div className="container-wrapper" ref={contactContainerRef}>
                     <div className="container-left">
                         <h1 className="container-title">Feel free to contact me!</h1>
                         <div className="container-info">
@@ -114,15 +157,15 @@ const Contact = ({setMenuOpen}) => {
                             form
                             below!</p>
                         <form ref={formRef} onSubmit={handleReview}>
-                            {inputs.map((inputVals) => (
+                            {inputs.map(inputVals => (
                                 <InputForm key={inputVals.id} {...inputVals} value={vals[inputVals.name]}
                                            onChange={handleChange} autoCapitalize="none"
-                                           onClick={handleInputClick}/>
+                                           onClick={handleInputClick} disabled={reviewActive}/>
                             ))}
-                            <label className="text-area-label">Your message</label>
+                            <label className="text-area-label">Your message (Max: 500 Characters)</label>
                             <textarea
                                 rows="10"
-                                maxLength="480"
+                                maxLength="500"
                                 onBlur={handleFocus}
                                 focused={focused.toString()}
                                 title="Please fill out this field."
@@ -131,30 +174,38 @@ const Contact = ({setMenuOpen}) => {
                                 required
                                 value={vals.message}
                                 onChange={handleChange}
+                                disabled={reviewActive}
                             />
                             <p className="error-message-textArea">Please enter your message above.
-                                Make sure you don't exceed past 480 characters.</p>
-                            <button className='review-button'
-                                    type="submit">
+                                Make sure you don't exceed past 500 characters.</p>
+                            <button
+                                className='review-button'
+                                type="submit"
+                                onClick={handleReviewButtonClick}
+                            >
                                 <p>Review</p>
                             </button>
                         </form>
+
                         <ToastContainer position="bottom-right" transition={Slide}/>
                     </div>
                 </div>
-                <div className='container-review fade-in'>
-                    <h2 className="modal-title">Review Your Submission</h2>
-                    <p className="modal-desc">Please review what you typed in the form down below. </p>
-                    <p className="modal-desc">
+                <div
+                    ref={containerReviewRef}
+                    className={`container-review ${reviewActive ? 'active' : ''}`}
+                >
+                    <h2 className="review-title">Review Your Submission</h2>
+                    <p className="review-desc">Please review what you typed in the form down below. </p>
+                    <p className="review-desc">
                         If you need to make any changes, click or tap the back button.
                     </p>
-                    <p className="modal-desc">
+                    <p className="review-desc">
                         Otherwise, wait 10 seconds before you can click or tap the submit button. This
                         is to prevent any
                         accidental submissions.
                     </p>
 
-                    {inputs.map((inputVals) => (
+                    {inputs.map(inputVals => (
                         <div key={inputVals.id} className="review-field">
                             <label className="input-label">{inputVals.label}:</label>
                             <p className="input-value">{vals[inputVals.name]}</p>
@@ -164,8 +215,12 @@ const Contact = ({setMenuOpen}) => {
                         <label className="input-label">Your message:</label>
                         <p className="input-value">{vals.message}</p>
                     </div>
-                    <div className="modal-buttons">
-                        <button className="back-button" type="button">
+                    <div className="review-form-buttons">
+                        <button
+                            className="back-button"
+                            type="button"
+                            onClick={handleBackButtonClick}
+                        >
                             <p>Back</p>
                         </button>
                         <button type="submit" className="submit-button">
